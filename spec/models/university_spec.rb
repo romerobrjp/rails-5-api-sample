0b5fd43014603774c4f01ab156e3d9ac516f1d80
Romero Medeiros <romeromedeiros5@gmail.com>
2017-11-20 13:00:54 -0300

    Testing acronym composition

--- spec/models/university_spec.rb
+++ spec/models/university_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe University, type: :model do
+  let(:university) { build(:university) }
+
   context 'atrributes' do
     it { is_expected.to respond_to(:name) }
     it { is_expected.to respond_to(:acronym) }
@@ -18,5 +20,11 @@ RSpec.describe University, type: :model do
     it { is_expected.to validate_presence_of(:state) }
     it { is_expected.to validate_uniqueness_of(:name) }
     it { is_expected.to validate_uniqueness_of(:acronym) }
+  end
+
+  context 'acronym' do
+    it 'is composed by the initial letters from the name attribute' do
+      expect(university.acronym).to eq(university.name.split(' ').map { |word| word[0] }.join)
+    end
   end
 end
