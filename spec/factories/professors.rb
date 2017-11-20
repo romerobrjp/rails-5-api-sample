bfb985b26636f7cf6d158f2beb31264c02764325
Romero Medeiros <romeromedeiros5@gmail.com>
2017-11-20 13:57:09 -0300

    Introducing traits and factories to test gender

--- spec/factories/professors.rb
+++ spec/factories/professors.rb
@@ -3,5 +3,17 @@ FactoryBot.define do
     name { Faker::Name.name }
     age { rand(18..90) }
     formation_area { Faker::Coffee.notes }
+    gender ['m', 'f'].sample
+
+    trait :male do
+      gender 'm'
+    end
+
+    trait :female do
+      gender 'f'
+    end
+
+    factory :male_professor, traits: [:male]
+    factory :female_professor, traits: [:female]
   end
 end
