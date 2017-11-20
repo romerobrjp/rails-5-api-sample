377c26000f58cbd480eb3ddceddd690cd0ba54f6
Romero Medeiros <romeromedeiros5@gmail.com>
2017-11-20 12:03:46 -0300

    Specifying join_table on many_to_many; Small Fix in numeracality validation;

--- app/models/discipline.rb
+++ app/models/discipline.rb
@@ -1,5 +1,5 @@
 class Discipline < ApplicationRecord
-  has_and_belongs_to_many :professors
+  has_and_belongs_to_many :professors, join_table: 'professors_disciplines'
 
   validates_presence_of :name
 end
--- app/models/professor.rb
+++ app/models/professor.rb
@@ -1,10 +1,10 @@
 class Professor < ApplicationRecord
   belongs_to :university
   belongs_to :campus
-  has_and_belongs_to_many :disciplines
+  has_and_belongs_to_many :disciplines, join_table: 'professors_disciplines'
 
   validates_presence_of :name, :age, :formation_area
   validates_uniqueness_of :name
-  validates_numericality_of :age, greater_than_or_equal_to: 18 
+  validates_numericality_of :age, greater_than: 18
   validates_format_of :formation_area, with: /[a-zA-Z(\,)?(\s)?]/
 end
