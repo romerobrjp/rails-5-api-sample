b86117bc443d09d3e164793ef1c5d8807bdd2ffc
Romero Medeiros <romeromedeiros5@gmail.com>
2017-11-20 13:56:03 -0300

    Gender attribute and migration

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -3,8 +3,9 @@ class Professor < ApplicationRecord
   belongs_to :campus
   has_and_belongs_to_many :disciplines, join_table: 'professors_disciplines'
 
-  validates_presence_of :name, :age, :formation_area
+  validates_presence_of :name, :age, :formation_area, :gender
   validates_uniqueness_of :name
   validates_numericality_of :age, greater_than: 18
   validates_format_of :formation_area, with: /[a-zA-Z(\,)?(\s)?]/
+  validates_inclusion_of :gender, in: ['m', 'f']
 end
---
+++ db/migrate/20171120161201_add_gender_to_professor.rb
@@ -0,0 +1,5 @@
+class AddGenderToProfessor < ActiveRecord::Migration[5.1]
+  def change
+    add_column :professors, :gender, :string
+  end
+end
