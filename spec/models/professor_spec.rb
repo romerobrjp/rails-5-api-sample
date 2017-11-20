3e1764c018d8ca94c52f5737d7e77b6bd109179c
Romero Medeiros <romeromedeiros5@gmail.com>
2017-11-20 13:58:30 -0300

    Gender specs

--- spec/models/professor_spec.rb
+++ spec/models/professor_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Professor, type: :model do
-  let(:professor1) { build(:professor) }
+  let(:professor) { build(:professor) }
 
   context 'attributes' do
     it { is_expected.to respond_to(:name) }
     it { is_expected.to respond_to(:age) }
+    it { is_expected.to respond_to(:gender) }
     it { is_expected.to respond_to(:formation_area) }
   end
 
@@ -18,23 +19,46 @@ RSpec.describe Professor, type: :model do
   context 'validations' do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:age) }
-    it { is_expected.to validate_numericality_of(:age) }
+    it { is_expected.to validate_presence_of(:gender) }
     it { is_expected.to validate_presence_of(:formation_area) }
+    it { is_expected.to validate_numericality_of(:age) }
+    it { is_expected.to validate_inclusion_of(:gender).in_array(['m', 'f']) }
   end
 
   context 'age' do
     it 'is a integer' do
-      expect(professor1.age).to be_instance_of(Integer)
+      expect(professor.age).to be_instance_of(Integer)
     end
 
     it 'is greater than 18 years old' do
-      expect(professor1.age).to be > 18
+      expect(professor.age).to be > 18
     end
   end
 
   context 'formation_area' do
     it 'has one or several values splitted by commas' do
-      expect(professor1.formation_area).to match(/[a-zA-Z(\,)?(\s)?]/)
+      expect(professor.formation_area).to match(/[a-zA-Z(\,)?(\s)?]/)
+    end
+  end
+
+  context 'gender' do
+    let(:male_professor) { build(:male_professor) }
+    let(:female_professor) { build(:female_professor) }
+
+    it 'is male or female' do
+      expect(male_professor.gender.downcase).to eq('m').or eq('f')
+    end
+
+    it 'male is m case_insensitive' do
+      expect(male_professor.gender.downcase).to eq('m')
+    end
+
+    it 'female is f case_insensitive' do
+      expect(female_professor.gender.downcase).to eq('f')
+    end
+
+    it 'can not be written in full' do
+      expect(professor.gender.length).to eq(1)
     end
   end
 end
