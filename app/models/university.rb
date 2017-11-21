4920346073fae3f04327d2325cafab50f2cbc36e
Romero Medeiros <romeromedeiros5@gmail.com>
2017-11-21 15:35:21 -0300

    Default scope example

--- app/models/university.rb
+++ app/models/university.rb
@@ -2,6 +2,8 @@ class University < ApplicationRecord
   has_many :professors
   has_many :campuses
 
+  default_scope -> { order(id: :asc) } # just an example of defatul_scope usage
+
   validates_presence_of :name, :acronym, :state
   validates_uniqueness_of :name, :acronym
 end
