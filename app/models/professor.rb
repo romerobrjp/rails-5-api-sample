8a88d5ab97c18d1dec6490f5d0e61a6c39543637
Romero Medeiros <romeromedeiros5@gmail.com>
2017-11-21 15:11:41 -0300

    Small fix in numericality validation

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -5,7 +5,7 @@ class Professor < ApplicationRecord
 
   validates_presence_of :name, :age, :formation_area, :gender
   validates_uniqueness_of :name
-  validates_numericality_of :age, greater_than: 18
+  validates_numericality_of :age, greater_than_or_equal_to: 18
   validates_format_of :formation_area, with: /[a-zA-Z(\,)?(\s)?]/
   validates_inclusion_of :gender, in: ['m', 'f']
 end
