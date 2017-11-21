483afccb553f1823807bccbd36b7c6609546838e
Romero Medeiros <romeromedeiros5@gmail.com>
2017-11-21 11:24:06 -0300

    Trait for university names with prepositions

--- spec/factories/universities.rb
+++ spec/factories/universities.rb
@@ -1,7 +1,12 @@
 FactoryBot.define do
   factory :university do
     name { Faker::University.name }
-    acronym { name.split(' ').map { |word| word[0] }.join }
+    acronym { name.split(' ').map { |word| word[0] }.join } # creates an acronym with the first letter of each word from name attribute
     state { Faker::StarWars.planet }
+  end
+
+  trait :prepositions_in_name do
+    name { Faker::Educator.course } # simulates a name with preposition(s) like 'Master of Architectural Technology'
+    acronym { name.split(' ').map { |word| word[0] if word[0] == word[0].upcase }.join } # creates acronym excluding the prepositions
   end
 end
