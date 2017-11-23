75c0f5968f90740ebe4ff8cf77dbef9cfd2c990c
Romero Medeiros <romeromedeiros5@gmail.com>
2017-11-22 23:03:35 -0300

    Discipline name must be unique

--- app/models/discipline.rb
+++ app/models/discipline.rb
@@ -2,4 +2,5 @@ class Discipline < ApplicationRecord
   has_and_belongs_to_many :professors, join_table: 'professors_disciplines'
 
   validates_presence_of :name
+  validates_uniqueness_of :name
 end
